633a9e5469b7e4eb60e83f538ced2cf5d6b1a98f
Mian Shah <mianhashimshah@Mians-MacBook-Air.local>
2017-09-04 17:30:31 -0700

    Initial Commit

---
+++ lab1_shah/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  lab1_shah
+//
+//  Created by Mian Hashim Shah on 9/4/17.
+//  Copyright © 2017 Mian Hashim Shah. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
